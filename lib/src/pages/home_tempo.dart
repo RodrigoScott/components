696d2bcf916c45447b9839e255e99e8b32d733e2
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-17 14:15:17 -0600

    Create list whit icons

--- lib/src/pages/home_tempo.dart
+++ lib/src/pages/home_tempo.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+
+  final  opciones = ['uno','dos','tres'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Componentes temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile( title: Text('List Title'),
-          ),
-          Divider(),
-          ListTile( title: Text('List Title'),
-          ),
-          Divider(),
-          ListTile( title: Text('List Title'),
-          ),
-          Divider(),
-
-        ],
+        children: _CrearItemCorto()
       ),
     );
   }
+
+ List<Widget> _crearItem(){
+
+    List<Widget> lista = new List<Widget>();
+
+    for(var opt in opciones){
+
+      final tempWidget = ListTile(
+        title: Text(opt),
+
+      );
+      lista..add(tempWidget)
+           ..add(Divider());
+
+    }
+    return lista;
+ }
+
+ List<Widget> _CrearItemCorto(){
+
+  return opciones.map((item){
+
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(item +' otra cosa'),
+          subtitle: Text('Otra cosa en el subtitulo'),
+          leading: Icon(Icons.access_time),
+          trailing: Icon(Icons.arrow_forward_ios),
+          onTap: (){},
+
+        ),
+        Divider()
+      ],
+    );
+
+  }).toList();
+
+ }
+
+
 }
