6680a61df28344bd4361428e02e918e327440b4b
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-17 13:15:37 -0600

    Add listview

--- lib/src/pages/home_tempo.dart
+++ lib/src/pages/home_tempo.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home temp'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Componentes temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile( title: Text('List Title'),
+          ),
+          Divider(),
+          ListTile( title: Text('List Title'),
+          ),
+          Divider(),
+          ListTile( title: Text('List Title'),
+          ),
+          Divider(),
+
+        ],
+      ),
     );
   }
 }
