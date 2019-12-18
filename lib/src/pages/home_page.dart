85ddd3d3e842723b7fb03d1e59cd3a8082d682bb
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-18 11:53:53 -0600

    use json data, create a FutureBuilder and use it

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -14,21 +14,42 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot){
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
 
-    print(menuProvider.opciones);
-
-    return ListView(
-      children: _listaItems(),
+      },
 
     );
+
+
   }
 
-  _listaItems() {
-    return [
-      ListTile(title: Text('Message')),
-      ListTile(title: Text('Message')),
-      ListTile(title: Text('Message')),
-    ];
+  List<Widget> _listaItems( List<dynamic> data) {
+
+    final List<Widget> opciones = [];
+    
+    data.forEach((opt){
+
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.cyan,),
+        trailing: Icon(Icons.keyboard_arrow_down, color: Colors.cyan ),
+        onTap: (){
+
+        },
+      );
+      opciones..add(widgetTemp)
+              ..add(Divider());
+
+    });
+
+    return opciones;
+
   }
 
 }
