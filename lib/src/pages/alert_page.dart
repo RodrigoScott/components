f831ffb422515f68a3c7e0829f82e73624fd43e3
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-19 13:06:04 -0600

    Widget Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -1,9 +1,23 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class alertPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: RaisedButton(
+            child: Text('Alertas'),
+            color: Colors.blue,
+            textColor: Colors.white,
+            shape: StadiumBorder(),
+
+            onPressed: () => _mostrarAlerta(context), //se tiene que hacer de esta forma cuando se mandan argumentos
+
+        ),
+
+      ),
       appBar: AppBar(
         title: Text('Ventana de alertas'),
       ),
@@ -15,4 +29,45 @@ class alertPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void _mostrarAlerta(BuildContext context) {
+  showDialog(
+      context: context,
+    barrierDismissible: true,
+    builder: (context){
+
+        return AlertDialog( //sive para crear una alerta
+          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la alerta'),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+                child: Text('Cancelar'),
+              onPressed: ()=> Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('OK'),
+              onPressed: (){
+
+                Navigator.of(context).pop();
+
+              },
+            ),
+          ],
+          //shape: StadiumBorder(),
+
+
+        );
+
+    }
+  );
+
 }
