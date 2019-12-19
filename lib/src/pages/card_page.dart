fc7ee40f7bd4b12b6c02cfb3b5b0eef30b871111
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-19 12:19:27 -0600

    customizing the style of the cards

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -18,8 +18,33 @@ floatingActionButton: FloatingActionButton(
         padding: EdgeInsets.all(20),
         children: <Widget>[
           _cardTipo1(),
-          SizedBox(height: 20),
+          SizedBox(height: 10),
           _cardTipo2(),
+          SizedBox(height: 10),
+          _cardTipo1(),
+          SizedBox(height: 10),
+          _cardTipo2(),
+          SizedBox(height: 10),
+          _cardTipo1(),
+          SizedBox(height: 10),
+          _cardTipo2(),
+          SizedBox(height: 10),
+          _cardTipo1(),
+          SizedBox(height: 10),
+          _cardTipo2(),
+          SizedBox(height: 10),
+          _cardTipo1(),
+          SizedBox(height: 10),
+          _cardTipo2(),
+          SizedBox(height: 10),
+          _cardTipo1(),
+          SizedBox(height: 10),
+          _cardTipo2(),
+          SizedBox(height: 10),
+          _cardTipo1(),
+          SizedBox(height: 10),
+          _cardTipo2(),
+          SizedBox(height: 10),
 
         ],
       ),
@@ -30,6 +55,8 @@ floatingActionButton: FloatingActionButton(
  Widget _cardTipo1() {
 
     return Card(
+      elevation: 5, //crear sombra
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -53,14 +80,16 @@ floatingActionButton: FloatingActionButton(
 }
 
 Widget _cardTipo2() {
-  return Card(
+
+  final card = Container(
+
     child: Column(
       children: <Widget>[
         FadeInImage(
           placeholder: AssetImage('assets/jar-loading.gif'),
           image: NetworkImage('https://cdn1.epicgames.com/ue/product/Screenshot/7-1920x1080-c4334b28ada3ae5c0ce2bf769794e8ca-1920x1080-c4334b28ada3ae5c0ce2bf769794e8ca.jpg'),
           fadeInDuration: Duration(milliseconds: 200),
-          height: 100,
+          height: 180,
           fit: BoxFit.cover,
         ),
         /*Image(
@@ -73,5 +102,26 @@ Widget _cardTipo2() {
 
       ],
     ),
+  );
+
+  return Container(
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: card,
+    ),
+    decoration: BoxDecoration(color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: <BoxShadow>[
+        BoxShadow(
+          color: Colors.black12,
+          blurRadius: 10.0,
+          spreadRadius: 10.0,
+          offset: Offset(2.0, 10.0),
+
+        )
+      ]
+      //color: Colors.cyan,
+    ),
+
   );
 }
