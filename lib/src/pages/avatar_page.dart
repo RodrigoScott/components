c2f7e11b9f53e03bb60d2d525c26b2fb088c4979
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-19 11:11:37 -0600

    Image and FadeIn Image

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -13,7 +13,6 @@ class avatarPage extends StatelessWidget {
           Navigator.pop(context);
         },
       ),
-
     );
   }
 }
--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -17,7 +17,10 @@ floatingActionButton: FloatingActionButton(
       body: ListView(
         padding: EdgeInsets.all(20),
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(height: 20),
+          _cardTipo2(),
+
         ],
       ),
 
@@ -47,4 +50,28 @@ floatingActionButton: FloatingActionButton(
 
     );
  }
+}
+
+Widget _cardTipo2() {
+  return Card(
+    child: Column(
+      children: <Widget>[
+        FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://cdn1.epicgames.com/ue/product/Screenshot/7-1920x1080-c4334b28ada3ae5c0ce2bf769794e8ca-1920x1080-c4334b28ada3ae5c0ce2bf769794e8ca.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+          height: 100,
+          fit: BoxFit.cover,
+        ),
+        /*Image(
+            image: NetworkImage('https://cdn1.epicgames.com/ue/product/Screenshot/7-1920x1080-c4334b28ada3ae5c0ce2bf769794e8ca-1920x1080-c4334b28ada3ae5c0ce2bf769794e8ca.jpg')
+        ),*/
+        Container(
+          padding: EdgeInsets.all(10),
+            child: Text('Info al pie de imágen')
+        ),
+
+      ],
+    ),
+  );
 }
