f23aa5457e9fd0482fe89d1bc603c6dbed83d51e
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-20 10:13:04 -0600

    Widget Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -5,9 +5,41 @@ class avatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(7.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('http://www.elclarinete.com.mx/wp-content/uploads/2018/10/10628343_941591029189799_4213220969187567108_n-844x500.jpg'),
+              radius: 20,
+
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('Rs'),
+              backgroundColor: Colors.black,
+            ),
+          )
+
+        ],
         title: Text('Ventana de avatares'),
       ),
-      floatingActionButton: FloatingActionButton(
+      
+      body: Container(
+
+        child: FadeInImage(
+          //alignment: ,
+      placeholder: AssetImage('assets/jar-loading.gif'),
+      fadeInDuration: Duration(milliseconds: 400),
+      image: NetworkImage('http://www.elclarinete.com.mx/wp-content/uploads/2018/10/10628343_941591029189799_4213220969187567108_n-844x500.jpg'),
+
+      ),
+        ),
+      
+      floatingActionButton: FloatingActionButton( //boton flotante
         child: Icon(Icons.reply),
         onPressed: (){
           Navigator.pop(context);
