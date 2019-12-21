1b581c7704d287e54894923bf4e8d93298c94283
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-21 11:46:07 -0600

    listview builder

---
+++ lib/src/pages/listview_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ListaPage extends StatefulWidget {
+  @override
+  _ListaPageState createState() => _ListaPageState();
+}
+
+class _ListaPageState extends State<ListaPage> {
+
+  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List View'),
+      ),
+      body: _crearLista(),
+
+    );
+  }
+
+ Widget _crearLista() {
+
+    return ListView.builder(
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index){
+          final _imagen = _listaNumeros[index];
+          return FadeInImage(
+            image: NetworkImage('https://i.picsum.photos/id/10$_imagen/2500/1656.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          );
+        },
+    );
+
+ }
+}
