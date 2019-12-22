18b728ff898f4fadcce915753e1ecddf3c6a00e6
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-21 22:43:23 -0600

    Infinite Scroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -7,7 +7,23 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  ScrollController _Scroll = new ScrollController();
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _agregar10();
+    _Scroll.addListener((){
+      if (_Scroll.position.pixels == _Scroll.position.maxScrollExtent){
+        _agregar10();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +39,7 @@ class _ListaPageState extends State<ListaPage> {
  Widget _crearLista() {
 
     return ListView.builder(
+      controller: _Scroll,
         itemCount: _listaNumeros.length,
         itemBuilder: (BuildContext context, int index){
           final _imagen = _listaNumeros[index];
@@ -32,6 +49,19 @@ class _ListaPageState extends State<ListaPage> {
           );
         },
     );
+
+ }
+
+ void _agregar10(){
+
+    for(var i=1; i<10; i++){
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+
+    setState(() {
+
+    });
 
  }
 }
